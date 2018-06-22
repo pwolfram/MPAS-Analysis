495e5a6b07c1179eb11bf3417a37e47dbc5a2392
Riley Brady <riley.brady@colorado.edu>
2018-06-22 13:08:53 -0600

    Include observations for BGC
    * Updated climatology_map_bgc.py to include setup for observations
    * Test case with NO3 WOA observations
    
    update config.lanl
    
    Fix BGC observations plotting
    
    * Add refField variables to plotting routine to not break subtask
    * Change name of lanl BGC config file
    * Change default output directory on lanl BGC config
    
    Syntax changes for climatology_map_bgc
    
    * Remove whitespace
    * Alter authors to RXB and PJW
    * Change indentation to follow PEP8 guidelines
    * Alter some difference color ranges in default config
    
    Add general functionality for BGC observations
    
    * Change climatology_map_bgc to include general obs file names
    
    Add CO2 flux observations
    
    * Add CO2 flux from Landschuetzer et al. 2016
    * Edit config.default to default to full BGC fields
    * Edit LANL BGC config file to test NO3 and CO2 flux with observations
    
    Add MPAS unit conversions to BGC
    
    * Simple implementation in climatology_map_bgc to convert MPAS CO2 flux
    to proper units for comparison to obs
    * Minor changes to config.default

--- mpas_analysis/ocean/climatology_map_bgc.py
+++ mpas_analysis/ocean/climatology_map_bgc.py
@@ -9,7 +9,6 @@ from __future__ import absolute_import, division, print_function, \
     unicode_literals
 
 import xarray as xr
-import datetime
 
 from mpas_analysis.shared import AnalysisTask
 
@@ -30,8 +29,7 @@ class ClimatologyMapBGC(AnalysisTask):  # {{{
 
     Authors
     -------
-    Phillip J. Wolfram, Luke Van Roekel, Xylar Asay-Davis, Milena Veneziani,
-    Riley X. Brady
+    Phillip J. Wolfram, Riley X. Brady
 
     """
     def __init__(self, config, mpasClimatologyTask, afieldName, ampasFieldName,
@@ -56,11 +54,11 @@ class ClimatologyMapBGC(AnalysisTask):  # {{{
         """
         # call the constructor from the base class (AnalysisTask)
         super(ClimatologyMapBGC, self).__init__(
-                config=config, taskName='climatologyMapBGC_' + afieldName,
-                componentName='ocean',
-                tags=['climatology', 'horizontalMap', 'BGC', afieldName])
-        
-        # CO2 flux has no verticle levels, throws error if you try to select
+            config=config, taskName='climatologyMapBGC_' + afieldName,
+            componentName='ocean',
+            tags=['climatology', 'horizontalMap', 'BGC', afieldName])
+
+        # CO2 flux has no vertical levels, throws error if you try to select
         # any. Can add any other flux-like variables to this list.
         if afieldName not in ['CO2_gas_flux']:
             iselValues = {'nVertLevels': 0}
@@ -75,7 +73,7 @@ class ClimatologyMapBGC(AnalysisTask):  # {{{
                                     'endYear')
         # Read in units (since BGC units are variable)
         units = config.getExpression(sectionName + '_' + afieldName, 'units',
-            elementType=str)
+                                     elementType=str)
 
         # read in what seasons we want to plot
         seasons = config.getExpression(sectionName, 'seasons')
@@ -93,7 +91,7 @@ class ClimatologyMapBGC(AnalysisTask):  # {{{
 
         # the variable mpasFieldName will be added to mpasClimatologyTask
         # along with the seasons.
-        remapClimatologySubtask = RemapMpasClimatologySubtask(
+        remapClimatologySubtask = RemapBGCClimatology(
             mpasClimatologyTask=mpasClimatologyTask,
             parentTask=self,
             climatologyName=afieldName,
@@ -102,31 +100,167 @@ class ClimatologyMapBGC(AnalysisTask):  # {{{
             seasons=seasons,
             iselValues=iselValues)
 
+        if refConfig is None:
+            refTitleLabel = 'Observations'
+
+            observationsDirectory = build_config_full_path(
+                config, 'oceanObservations',
+                '{}Subdirectory'.format(afieldName))
+
+            obsFileName = \
+                "{}/{}_1.0x1.0degree.nc" .format(
+                    observationsDirectory, afieldName)
+
+            observationsLabel = config.getExpression(sectionName + '_' +
+                afieldName, 'observationsLabel', elementType=str)
+            refFieldName = afieldName
+            outFileLabel = afieldName + observationsLabel
+            galleryName = 'Observations: ' + observationsLabel
+
+            remapObservationsSubtask = RemapObservedBGCClimatology(
+                parentTask=self, seasons=seasons, fileName=obsFileName,
+                outFilePrefix=refFieldName,
+                comparisonGridNames=comparisonGridNames)
+            self.add_subtask(remapObservationsSubtask)
+            diffTitleLabel = 'Model - Observations'
+        else:
+            remapObservationsSubtask = None
+            refRunName = refConfig.get('runs', 'mainRunName')
+            galleryName = None
+            refTitleLabel = 'Ref: {}'.format(refRunName)
+
+            refFieldName = ampasFieldName
+            outFileLabel = afieldName
+            diffTitleLabel = 'Main - Reference'
+
+
         for comparisonGridName in comparisonGridNames:
             for season in seasons:
                 # make a new subtask for this season and comparison grid
                 subtask = PlotClimatologyMapSubtask(self, season,
                                                     comparisonGridName,
                                                     remapClimatologySubtask,
-                                                    None,
+                                                    remapObservationsSubtask,
                                                     refConfig)
 
                 subtask.set_plot_info(
-                        outFileLabel=afieldName,
-                        fieldNameInTitle=afieldName,
-                        mpasFieldName=ampasFieldName,
-                        refFieldName=None,
-                        refTitleLabel=None,
-                        unitsLabel=units,
-                        imageCaption='Mean ' + afieldName,
-                        galleryGroup='Sea Surface Biogeochemistry',
-                        groupSubtitle=None,
-                        groupLink=afieldName,
-                        galleryName=afieldName,
-                        diffTitleLabel=None)
+                    outFileLabel=afieldName,
+                    fieldNameInTitle=afieldName,
+                    mpasFieldName=ampasFieldName,
+                    refFieldName=refFieldName,
+                    refTitleLabel=refTitleLabel,
+                    unitsLabel=units,
+                    imageCaption='Mean ' + afieldName,
+                    galleryGroup='Sea Surface Biogeochemistry',
+                    groupSubtitle=None,
+                    groupLink=afieldName,
+                    galleryName=afieldName,
+                    diffTitleLabel=diffTitleLabel)
 
                 self.add_subtask(subtask)
         # }}}
+    # }}}
+
+class RemapBGCClimatology(RemapMpasClimatologySubtask): # {{{
+    """
+    Apply unit conversions to native model output to align with observations.
+    """
+    # Authors
+    # -------
+    # Riley X. Brady
+
+    def customize_remapped_climatology(self, climatology, comparisonGridName,
+                                       season): # {{{
+        """
+        Currently, convert gas flux from native units to mol/m2/yr
+
+        Parameters
+        ----------
+        climatology : ``xarray.Dataset``
+            The MPAS climatology data set that has been remapped
+
+        comparisonGridNames : {'latlon', 'antarctic'}
+            The name of the comparison grid to use for remapping
+
+        season : str
+            The name of the season to be remapped
+
+        Returns
+        -------
+        climatology : ``xarray.Dataset``
+            The same data set with any custom fields added or modifications
+            made
+        """
+        # Authors
+        # -------
+        # Riley X. Brady
+
+        fieldName = self.variableList[0]
+        # Convert CO2 gas flux from native mmol/m2 m/s to mol/m2/yr for
+        # comparison to the SOM-FFN product
+        if 'CO2_gas_flux' in fieldName:
+            conversion = -1 * (60 * 60 * 24 * 365.25) / 10**3
+            climatology[fieldName] = conversion * climatology[fieldName]
+        return climatology # }}}
+
+    # }}}
+
+class RemapObservedBGCClimatology(RemapObservedClimatologySubtask): # {{{
+    """
+    A subtask for reading and remapping BGC observations
+    """
+    # Authors
+    # -------
+    # Riley X. Brady
+
+    def get_observation_descriptor(self, fileName): # {{{
+        '''
+        get a MeshDescriptor for the observation grid
+
+        Parameters
+        ----------
+        fileName : str
+            observation file name describing the source grid
+
+        Returns
+        -------
+        obsDescriptor : ``MeshDescriptor``
+            The descriptor for the observation grid
+        '''
+        # Authors
+        # -------
+        # Riley X. Brady
+
+        # create a descriptor of the observation grid using the lat/lon
+        # coordinates
+        obsDescriptor = LatLonGridDescriptor.read(fileName=fileName,
+                                                  latVarName='lat',
+                                                  lonVarName='lon')
+        return obsDescriptor # }}}
+
+    def build_observational_dataset(self, fileName): # {{{
+        '''
+        read in the data sets for observations, and possibly rename some
+        variables and dimensions
+
+        Parameters
+        ----------
+        fileName : str
+            observation file name
+
+        Returns
+        ------
+        dsObs : ``xarray.Dataset``
+            The observational dataset
+        '''
+        # Authors
+        # -------
+        # Riley X. Brady
+
+        # Obs are pre-processed, so nothing needed to be done here.
+        dsObs = xr.open_dataset(fileName)
+        return dsObs # }}}
+
     # }}}
 
 # vim: foldmethod=marker ai ts=4 sts=4 et sw=4 ft=python
